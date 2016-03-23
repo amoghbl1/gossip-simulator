d22cb75274b857726f9227dc538780a0f12568b8
Amogh Pradeep <amoghbl1@gmail.com>
2016-03-24 03:49:58 +0530

    Interface need not import *

--- GossipInterface.java
+++ GossipInterface.java
@@ -1,4 +1,5 @@
-import java.rmi.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface GossipInterface extends java.rmi.Remote {
         void hearGossip(byte[] gossipProtobuf) throws RemoteException;
